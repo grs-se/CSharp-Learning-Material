f346d6ea63b298e185c15dfaa1f740828f13ec36
grs-se <george_ricesmith@hotmail.com>
2023-10-18 19:13:33 +0100

    Create customer and accounts menu

--- 02. Bank Project - Getting Started/HarshaBank/HarshaBank.Presentation/Program.cs
+++ 02. Bank Project - Getting Started/HarshaBank/HarshaBank.Presentation/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
     //Application execution starts here
     static void Main()
@@ -11,25 +13,102 @@
         string userName = null, password = null;
 
         //read userName from keyboard
-        System.Console.Write("Username:");
+        System.Console.Write("Username: ");
         userName = System.Console.ReadLine();
 
 
         //read password from keyboard only if username is entered
         if (userName != "")
         {
-            System.Console.Write("Password:");
+            System.Console.Write("Password: ");
             password = System.Console.ReadLine();
         }
 
         //check username and password
         if (userName == "system" && password == "manager")
         {
-            System.Console.WriteLine("TO DO: Main menu here");
+            //declare variable to store menu choice
+            int mainMenuChoice = -1;
+
+            do
+            {
+                System.Console.WriteLine("\n:::Main menu:::");
+                System.Console.WriteLine("1. Customers");
+                System.Console.WriteLine("2. Accounts");
+                System.Console.WriteLine("3. Funds Transfer");
+                System.Console.WriteLine("4. Funds Transfer Statement");
+                System.Console.WriteLine("5. Account Statement");
+                System.Console.WriteLine("0. Exit");
+
+                //accept menu choice from keyboard
+                System.Console.WriteLine("Enter choice: ");
+                mainMenuChoice = int.Parse(System.Console.ReadLine());
+
+                //switch-base to check menu choice
+                switch (mainMenuChoice)
+                {
+                    case 1: CustomersMenu(); break;
+                    case 2: AccountsMenu(); break;
+                    case 3: //TO DO: Display funds transfer menu
+                        break;
+                    case 4: //TO DO: Display funds transfer statement menu
+                        break;
+                    case 5: //TO DO: Display account statement menu
+                        break;
+                }
+            } while (mainMenuChoice != 0);
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid username or password");
         }
 
         //about to exit
         System.Console.WriteLine("Thank you! Visit again");
         System.Console.ReadKey();
+    }
+
+    static void AccountsMenu()
+    {
+        //variable to store accounts menu choice
+        int accountsMenuChoice = -1;
+
+        //do-while loop starts
+        do
+        {
+            //print  accounts menu
+            System.Console.WriteLine("\n:::Accounts menu:::");
+            System.Console.WriteLine("1. Add Account");
+            System.Console.WriteLine("2. Delete Account");
+            System.Console.WriteLine("3. Update Account");
+            System.Console.WriteLine("4. View Accounts");
+            System.Console.WriteLine("0. Back to Main Menu");
+
+            //accept accounts menu choice
+            System.Console.Write("Enter choice: ");
+            accountsMenuChoice = System.Convert.ToInt32(System.Console.ReadLine());
+        } while (accountsMenuChoice != 0);
+    }
+
+    static void CustomersMenu()
+    {
+        //variable to store customers menu choice
+        int customerMenuChoice = -1;
+
+        //do-while loop starts
+        do
+        {
+            //print customers menu
+            System.Console.WriteLine("\n:::Customers menu:::");
+            System.Console.WriteLine("1. Add Customer");
+            System.Console.WriteLine("2. Delete Customer");
+            System.Console.WriteLine("3. Update Customer");
+            System.Console.WriteLine("4. View Customers");
+            System.Console.WriteLine("0. Back to Main Menu");
+
+            //accept customers menu choice
+            System.Console.Write("Enter choice: ");
+            customerMenuChoice = System.Convert.ToInt32(System.Console.ReadLine());
+        } while (customerMenuChoice != 0);
     }
 }
