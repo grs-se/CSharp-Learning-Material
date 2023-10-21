71cb5677f49523c83583d2cce7397ce411f23ef3
grs-se <george_ricesmith@hotmail.com>
2023-10-21 21:34:22 +0100

    Fields

---
+++ 04. Fields/ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿public class Product
+{
+    //fields
+    public int productID;
+    public string productName;
+    public double cost;
+    public int quantityInStock;
+    public static int TotalNoProducts;
+    public const string CategoryName = "Electronics";
+    public readonly string dateOfPurchase;
+
+    //constructor
+    //outside of constructor readonly field cannot be modified
+    public Product()
+    {
+        dateOfPurchase = System.DateTime.Now.ToShortDateString();
+    }
+}
+
