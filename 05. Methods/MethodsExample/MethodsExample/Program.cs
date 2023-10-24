328bf10f19c5c98fd9c0fd38bacba84750c4ca5d
grs-se <george_ricesmith@hotmail.com>
2023-10-24 16:33:28 +0100

    Named Arguments

--- 05. Methods/MethodsExample/MethodsExample/Program.cs
+++ 05. Methods/MethodsExample/MethodsExample/Program.cs
@@ -36,8 +36,7 @@
         product3.SetQuantityInStock(800);
 
         //call methods
-        //doens't return any value so cannot print anything to console with Console.WriteLine()
-        product1.CalculateTax(9.2);
+        product1.CalculateTax(percentage: 9.2);
         product2.CalculateTax(7.4);
         product3.CalculateTax();
 
